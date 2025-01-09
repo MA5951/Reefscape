79d93cbe7005894ec436b0943488b7f8461ba424
GalDotan <gldotan@gmail.com>
2025-01-10 01:35:21 +0200

    Create DashboardPIDTuner.java

---
+++ src/main/java/com/ma5951/utils/DashBoard/DashboardPIDTuner.java
@@ -0,0 +1,31 @@
+
+package com.ma5951.utils.DashBoard;
+
+import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
+import java.util.function.Function;
+
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+
+public class DashboardPIDTuner implements Sendable {
+
+    private String name;
+    private Consumer<Double> kPConsumer;
+    private Consumer<Double> kIConsumer;
+    private Consumer<Double> kDConsumer;
+
+    public DashboardPIDTuner(String name , Function<Double , Function<Double , Consumer<Double>>> it) {
+        this.name = name;
+
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType(name + "PIDCTuner");
+        builder.addDoubleProperty("kP", () -> 1, null);
+        builder.addDoubleProperty("kI", () -> 2, null);
+        builder.addDoubleProperty("kD", () -> 3, null);
+        builder.addDoubleProperty("Set Point", null, null);
+    }
+}
